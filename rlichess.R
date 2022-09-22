4ef24fb7676a0228a071e695a2553559a01dedea
Hiroki Yagi <hiro.yagi0245@gmail.com>
2022-09-22 16:33:09 +0900

    add access_token

--- rlichess.R
+++ rlichess.R
@@ -2,8 +2,16 @@ library(tidyverse)
 library(httr)
 library(jsonlite)
 
+readRenviron("./.env")
+access_token <- Sys.getenv("access_token")
+
 data <- GET("https://lichess.org/api/games/user/h8gi",
   add_headers(Accept = "application/x-ndjson"),
   query = list(max = 10, perfType = "bullet", opening = "true", moves = "false"))
 
 game_json <- data |> content(as="text", encoding = "utf-8") |> textConnection() |> stream_in()
+
+## win or lose
+## https://github.com/lichess-org/scalachess/blob/0a7d6f2c63b1ca06cd3c958ed3264e738af5c5f6/src/main/scala/Status.scala#L50
+## val finishedWithWinner = List(Mate, Resign, Timeout, Outoftime, Cheat, NoStart, VariantEnd)
+finished_with_winner <- c("mate", "resign", "timeout", "outoftime", "cheated", "noStart", "variantEnd")
