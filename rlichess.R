61be8fcc32e3152fb401b2d746f6a994c0245b8a
Hiroki Yagi <hiro.yagi0245@gmail.com>
2022-09-22 17:44:07 +0900

    using curl

--- rlichess.R
+++ rlichess.R
@@ -1,7 +1,13 @@
 library(tidyverse)
+library(httr)
 library(curl)
-## library(httr)
+library(urltools)
 library(jsonlite)
+
+## win or lose
+## https://github.com/lichess-org/scalachess/blob/0a7d6f2c63b1ca06cd3c958ed3264e738af5c5f6/src/main/scala/Status.scala#L50
+## val finishedWithWinner = List(Mate, Resign, Timeout, Outoftime, Cheat, NoStart, VariantEnd)
+finished_with_winner <- c("mate", "resign", "timeout", "outoftime", "cheated", "noStart", "variantEnd")
 
 readRenviron("./.env")
 access_token <- Sys.getenv("access_token")
@@ -9,23 +15,20 @@ access_token <- Sys.getenv("access_token")
 pr <- GET("https://lichess.org/api/account",
   add_headers("Authorization" = str_c("Bearer ", access_token)))
 
-
-data <- GET("https://lichess.org/api/games/user/h8gi",
-  add_headers("Authorization" = str_c("Bearer ", access_token)),
-  accept("application/x-ndjson"),
-  query = list(max = 10, perfType = "bullet", opening = "true", moves = "false"))
-
-game_json <- data |> content(as="text", encoding = "utf-8") |> textConnection() |> stream_in()
-
-## win or lose
-## https://github.com/lichess-org/scalachess/blob/0a7d6f2c63b1ca06cd3c958ed3264e738af5c5f6/src/main/scala/Status.scala#L50
-## val finishedWithWinner = List(Mate, Resign, Timeout, Outoftime, Cheat, NoStart, VariantEnd)
-finished_with_winner <- c("mate", "resign", "timeout", "outoftime", "cheated", "noStart", "variantEnd")
-
 ## R ndjson as tibble
 
-con <- curl("https://lichess.org/api/games/user/h8gi")
+h <- new_handle() |> handle_setheaders(
+  "Authorization" = str_c("Bearer ", access_token),
+  "Accept" = "application/x-ndjson"
+)
 
-out <- readLines(con, n = 3)
+url <- "https://lichess.org/api/games/user/h8gi" |>
+  param_set(key = "perfType", value = "bullet") |>
+  param_set(key = "opening", value = "true") |>
+  param_set(key = "moves", value = "false") |>
+  param_set(key = "max", value = 10)
 
-close(con)
+
+con <- curl(url, handle = h)
+
+game_json <- stream_in(con = con)
