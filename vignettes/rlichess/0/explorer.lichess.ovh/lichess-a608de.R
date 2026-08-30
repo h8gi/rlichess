c0f58dd9481ab4a1bc72b37367a9d2342357a2cc
hiroki yagi <hiro.yagi0245@gmail.com>
2026-08-31 00:15:36 +0900

    fix(vignettes): track .R mock fixture files in vignettes/.gitignore

---
+++ vignettes/rlichess/0/explorer.lichess.ovh/lichess-a608de.R
@@ -0,0 +1,9 @@
+structure(list(method = "GET", url = "https://explorer.lichess.ovh/lichess?variant=standard&play=e4%2Cc5&ratings=2000%2C2200%2C2500&speeds=blitz%2Crapid", 
+    status_code = 401L, headers = structure(list(server = "nginx", 
+        date = "Sun, 30 Aug 2026 07:02:33 GMT", `content-type` = "text/html; charset=utf-8", 
+        `content-length` = "172", vary = "Origin", `access-control-allow-origin` = "*", 
+        `access-control-allow-methods` = "GET,OPTIONS", `access-control-allow-headers` = "Accept,If-Modified-Since,Cache-Control,X-Requested-With,Authorization"), class = "httr2_headers"), 
+    body = charToRaw("<html>\r\n<head><title>401 Authorization Required</title></head>\r\n<body>\r\n<center><h1>401 Authorization Required</h1></center>\r\n<hr><center>nginx</center>\r\n</body>\r\n</html>\r\n"), 
+    timing = c(redirect = 0, namelookup = 0.001839, connect = 0.270689, 
+    pretransfer = 0.55022, starttransfer = 0.812568, total = 0.812642
+    ), cache = new.env(parent = emptyenv())), class = "httr2_response")
